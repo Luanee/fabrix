fd808889fa71b638d6a3a79208aaf002d644fb7d
Pit Nahrstedt <pit_nahrstedt@web.de>
2025-08-28 21:26:33 +0200

    feat(functions): Added collection functions

--- src/fabrix/functions/__init__.py
+++ src/fabrix/functions/__init__.py
@@ -1,9 +1,11 @@
+from fabrix.functions import collections as CollectionFunction
 from fabrix.functions import conversions as ConversionFunctions
 from fabrix.functions import dates as DateFunctions
 from fabrix.functions import maths as MathFunctions
 from fabrix.functions import strings as StringFunctions
 
 __all__ = [
+    "CollectionFunction",
     "ConversionFunctions",
     "DateFunctions",
     "MathFunctions",
