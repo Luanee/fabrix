e64b6fe267f6f0317792ac472efd61842124786c
Pit Nahrstedt <pit_nahrstedt@web.de>
2025-08-28 21:20:40 +0200

    feat(functions): Added string functions

---
+++ src/fabrix/functions/__init__.py
@@ -0,0 +1,30 @@
+from fabrix.functions.strings import (
+    concat,
+    ends_with,
+    guid,
+    index_of,
+    last_index_of,
+    replace,
+    split,
+    starts_with,
+    substring,
+    to_lower,
+    to_upper,
+    trim,
+)
+
+__all__ = [
+    # string functions
+    "concat",
+    "ends_with",
+    "guid",
+    "index_of",
+    "last_index_of",
+    "replace",
+    "split",
+    "starts_with",
+    "substring",
+    "to_lower",
+    "to_upper",
+    "trim",
+]
